f9605f21d6d9c9676ec20a3e528767edb0a11678
MobSlicer152 <brambleclaw1414@gmail.com>
2023-08-30 12:10:23 -0400

    Remove unused stuff

--- _manifest.lua
+++ _manifest.lua
@@ -5,12 +5,6 @@
 
 return {
 	'consoles.lua',
-	'durango.lua',
-	'xbox.lua',
-	'xboxone_gdk.lua',
-	'assets/Logo.png',
-	'assets/SmallLogo.png',
-	'assets/SplashScreen.png',
-	'assets/StoreLogo.png',
-	'assets/WideLogo.png',
+	'desktop.lua',
+	'scarlett.lua'
 }
